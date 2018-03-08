91e5ab80a538c831248ef22906f7702391d610d1
albertosouza <alberto.souza.99@gmail.com>
2018-03-07 23:16:47 -0300

    add share blocks

--- files/public/script.js
+++ files/public/script.js
@@ -2775,3 +2775,11 @@ if (typeof jQuery === 'undefined') {
     $('.match-grid-col-scroll').matchHeight();
   });
 })(jQuery);
+
+(function($) {
+  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    $('body').addClass('is-mobile');
+  } else {
+    $('body').addClass('not-is-mobile');
+  }
+})(jQuery);
--- files/scripts/main.js
+++ files/scripts/main.js
@@ -8,3 +8,11 @@
     $('.match-grid-col-scroll').matchHeight();
   });
 })(jQuery);
+
+(function($) {
+  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    $('body').addClass('is-mobile');
+  } else {
+    $('body').addClass('not-is-mobile');
+  }
+})(jQuery);
